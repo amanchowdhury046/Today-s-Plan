3ddc80de4012a1f0930a23c1e298f257009cec8d
Aman Chowdhury <amanchowdhury046@gmail.com>
2022-01-01 22:29:46 +0530

    Updates

---
+++ precache-manifest.2f4776ebf3a7bf31d1e0416c2bc78752.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ec669ff3ea540fac6ed",
+    "url": "/Today-s-Plan/static/css/main.7ca40243.chunk.css"
+  },
+  {
+    "revision": "7ec669ff3ea540fac6ed",
+    "url": "/Today-s-Plan/static/js/main.7ec669ff.chunk.js"
+  },
+  {
+    "revision": "46ba77eab6b005b961f9",
+    "url": "/Today-s-Plan/static/css/1.1c74ccce.chunk.css"
+  },
+  {
+    "revision": "46ba77eab6b005b961f9",
+    "url": "/Today-s-Plan/static/js/1.46ba77ea.chunk.js"
+  },
+  {
+    "revision": "07238c54be9f607e6d40",
+    "url": "/Today-s-Plan/static/js/runtime~main.07238c54.js"
+  },
+  {
+    "revision": "7a6e7674e6df2ab77d85e165ada6c44f",
+    "url": "/Today-s-Plan/index.html"
+  }
+];
